2f07c0316c5da3a611b3e5053c3f99755cebeb00
jimmy <sergii.pryhodko@gmail.com>
2019-04-19 23:30:19 +0300

    added regex annotation on field

--- src/test/java/domain/positive/DtoWithPositiveIntegerConstraint.java
+++ src/test/java/domain/positive/DtoWithPositiveIntegerConstraint.java
@@ -1,6 +1,7 @@
 package domain.positive;
 
 import validation.constraints.PositiveInteger;
+import validation.constraints.RegexConstraint;
 import validation.constraints.StringLength;
 import validation.domain.Dto;
 
@@ -8,6 +9,7 @@ public class DtoWithPositiveIntegerConstraint implements Dto {
 
     @StringLength
     @PositiveInteger
+    @RegexConstraint(pattern = "\\d")
     private Integer age;
 
     public void setAge(Integer age) {
